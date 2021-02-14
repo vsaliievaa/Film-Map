b82117f81769eb0016ba090b1370f908ac75c552
vsaliievaa <73236013+vsaliievaa@users.noreply.github.com>
2021-02-14 22:12:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,64 @@
 """films geo html-map"""
+import folium
+from geopy.geocoders import Nominatim
+from geopy.exc import GeocoderUnavailable
 import math
+import re
 
 
-def read_locations():
+def processing_locations(locations: list) -> list:
     """
-    Takes path to a file with locations.
+    Calculates the cordinates for locations, appends them to locations list
+    and returns the list.
     """
-    lst = []
-    with open('C:/Users/User/Downloads/locations.list', 'r', encoding='ISO-8859-1') as file:
+    for i in range(len(locations)):
+        coords = coordinates(locations[i][1])
+        if coords != 'Not found':
+            locations[i].append(coords)
+    return locations
+
+
+def read_locations(year: int) -> list:
+    """
+    Takes path to a file with locations, returns a list of all films, that
+    match the year, entered by user, and are available for geocoding.
+    """
+    locations = []
+    with open('D:/R/loc.list', 'r', encoding='ISO-8859-1') as file:
         for _ in range(14):
             file.readline()
-        for _ in range(2):
+        for _ in range(1000):
             line = file.readline()
-            line = line.strip().split('\t')
-            lst.append((line[0], line[-1]))
-    return lst
+            if re.findall(str(year), line):
+                line = line.strip().split('\t')
+                if len(line[-1].split("(")) == 1:
+                    f_line = list(filter(None, line))
+                    locations.append([f_line[0], f_line[1:]])
+    return locations
 
 
-def haversine(lat_1, lon_1, lat_2, lon_2):
+def coordinates(place: str) -> tuple:
+    """
+    Defines the coordinates of the given location using geopy, returns 'Not found'
+    if coordinates of the given place cannot be found.
+    """
+    try:
+        geolocator = Nominatim(user_agent="Film-Map")
+        location = geolocator.geocode(place)
+        if hasattr(location, 'latitude') and location.latitude is not None:
+            if hasattr(location, 'longitude') and location.longitude is not None:
+                return ((location.latitude, location.longitude))
+        return 'Not found'
+    except:
+        'GeocoderUnavailable'
+
+
+def haversine(lat_1: str, lon_1: str, lat_2: str, lon_2: str) -> str:
     """
     Calculates the distance between two points on the Earth, given these points` coordinates.
     """
     lat_1, lon_1, lat_2, lon_2 = map(
-        math.radians, [lat_1, lon_1, lat_2, lon_2])
+        math.radians, [float(lat_1), float(lon_1), float(lat_2), float(lon_2)])
 
     lon = lon_2 - lon_1
     lat = lat_2 - lat_1
@@ -32,4 +68,49 @@ def haversine(lat_1, lon_1, lat_2, lon_2):
 
     distance = 2 * 6371 * math.asin(math.sqrt(haver))
 
-    return distance
+    return str(distance)
+
+
+def sort_locations(locations: list) -> list:
+    """
+    Sorts all locations of the given year by distance and returns them.
+    """
+    locations.sort(key=lambda x: float(x[-1]))
+    return locations
+
+
+# def create_map(place):
+#    coords = coordinates(place)
+#    lat = coords[0]
+#    lon = coords[1]
+#    geomap = folium.Map(zoom_start=10)
+#    fg = folium.FeatureGroup(name="Test Film Map")
+#    fg.add_child(folium.Marker(
+#        location=[lat, lon], popup='Test', icon=folium.Icon()))
+#    geomap.add_child(fg)
+#    geomap.save('D:/R/filmmap.html')
+
+def main():
+    """
+    Main function of the module. As of now, allows the user to enter their data and returns the result. 
+    """
+    print('Enter a year you would like to form a map for: ', end='')
+    year = int(input())
+    print('Now enter your location (format: lat, lon): ', end='')
+    lat, lon = input().split(',')
+    print("Generating your map, please wait...")
+    locations = processing_locations(read_locations(year))
+    for i in locations:
+        if not isinstance(i[-1], list):
+            coords = i[-1]
+            try:
+                coords = i[-1]
+                distance = haversine(lat, lon, coords[0], coords[1])
+                i.append(distance)
+            except:
+                "'NoneType' object is not subscriptable"
+    result = []
+    for i in range(len(locations)):
+        if not isinstance(locations[i][-1], list):
+            result.append(locations[i])
+    return sort_locations(result)
